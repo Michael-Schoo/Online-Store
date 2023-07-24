e197d4eeff6cd0ceea405ffa6d98ef121a7b5c74
Michael H <56214343+RiskyMH@users.noreply.github.com>
2023-07-25 09:07:23 +1000

    Initial database

---
+++ src/lib/prisma.ts
@@ -0,0 +1,9 @@
+import { PrismaClient } from '@prisma/client'
+
+// @ts-expect-error global variable
+const prisma: PrismaClient = global.prisma || new PrismaClient()
+
+// @ts-expect-error global variable
+if (process.env.NODE_ENV === 'development') global.prisma = prisma
+
+export default prisma
