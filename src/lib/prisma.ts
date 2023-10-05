6db7815595a33428e8a1c5c99e76f62e41b9127e
Michael H <84158733+Michael-Schoo@users.noreply.github.com>
2023-10-05 15:35:07 +1100

    More betterness with prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,7 +1,20 @@
-import { PrismaClient } from "@prisma/client"
+// Import required dependencies
+import { connect } from '@planetscale/database';
+import { PrismaPlanetScale } from '@prisma/adapter-planetscale';
+import { PrismaClient } from '@prisma/client';
+import { env } from 'process';
+
+function getPrisma() {
+    // Initialize Prisma Client with the PlanetScale serverless database driver
+    const connection = connect({ url: env.DATABASE_URL });
+    const adapter = new PrismaPlanetScale(connection);
+    const prisma = new PrismaClient({ adapter, log: ["query"] });
+
+    return prisma 
+}
 
 // @ts-expect-error global variable
-const prisma: PrismaClient = global.prisma || new PrismaClient({ log: ["query"] })
+const prisma: PrismaClient = global.prisma || getPrisma()
 
 // @ts-expect-error global variable
 if (process.env.NODE_ENV === "development") global.prisma = prisma
